c1b489bebdf0eb3430e720f9150664a071b8940b
HyungJun-An <HyungJun-An@github.com>
2024-04-01 03:32:40 -0400

    20240401

---
+++ L1/src/P1.c
@@ -0,0 +1,8 @@
+#include <stdio.h>
+#include <stdbool.h>
+#include <stdlib.h>
+
+int solution(const char* friends[], size_t friends_len, const char* gifts[], size_t gifts_len) {
+    int answer = 0;
+    return answer;
+}
